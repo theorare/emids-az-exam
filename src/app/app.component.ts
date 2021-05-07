77771a0ee7e1121b3a9a0686e6267567922f0cc2
“Bharath <theorare@gmail.com>
2021-05-07 23:51:46 +0530

    Fields added, validations done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl, AbstractControl, ValidatorFn  } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder){}
+
   title = 'emids-az-exam';
+  public patientForm: FormGroup;
+
+  ngOnInit(): void {
+    this.patientForm = this.formBuilder.group({
+      patientId: [''],
+      patientName: ['', [Validators.required,
+      Validators.minLength(3),
+       Validators.maxLength(50),
+        Validators.pattern('^[_A-z0-9]*((.-|\s)*[_A-z0-9])*$')]],
+      patientDob: ['', [Validators.required]],
+      patientEmail: [''],
+      patientImage: ['', this.urlValidator]
+  });
+
+  }
+
+  addPatient(): any {
+    return this.patientForm;
+  }
+
+  get patient(): any {
+    return this.patientForm.controls;
+  }
+
+  urlValidator: ValidatorFn = (control: AbstractControl) => {
+    let validUrl = true;
+
+    try {
+      // tslint:disable-next-line: no-unused-expression
+      validUrl = new RegExp('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?').test(control.value);
+    } catch {
+      validUrl = false;
+    }
+
+    return validUrl ? null : { invalidUrl: true };
+  }
+
 }
